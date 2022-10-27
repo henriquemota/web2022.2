fdfec3c54a2999b4809c17b4a9a86057a2dbf7e9
henriquemota <mota.henrique@gmail.com>
2022-10-27 20:35:36 -0300

    posts listados

--- react/navegacao/src/pages/posts.jsx
+++ react/navegacao/src/pages/posts.jsx
@@ -1,12 +1,43 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { BASE_URL } from '../constants'
+
 function PostList(props) {
-  console.log(props)
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/posts`)
+      .then(({ data, status }) => status === 200 ? setPosts(data) : setPosts([]))
+      .catch((err) => alert(err))
+  }, [])
+
   return (
-    <h1>Postlist</h1>
+    <section className='container'>
+      <table className='table table-striped'>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>title</th>
+            <th>post</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map((el, ix) => {
+            return (
+              <tr key={ix}>
+                <td>{el.id}</td>
+                <td>{el.title}</td>
+                <td>{el.body}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </section>
   )
 }
 
 function Post(props) {
-  console.log(props)
   return (
     <h1>Post</h1>
   )
